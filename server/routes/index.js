a55de00de3437d0c2c53f2e5c3a1476a01beb86b
luisnueda <nueda.luis@gmail.com>
2018-05-08 13:26:59 +0200

    lector rfid

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const rc522 = require("rc522");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/rfid', (req, res, next) => {
+
+  console.log('Ready!!!');
+  rc522(function (rfidSerialNumber) {
+    console.log(rfidSerialNumber);
+  });
+})
 
 module.exports = router;
