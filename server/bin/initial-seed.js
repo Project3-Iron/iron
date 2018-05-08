e0e939641c19acf26d2875793ef4da9d70a5fdf0
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-08 16:01:19 +0200

    Modelo DONE (a revisar)

---
+++ server/bin/initial-seed.js
@@ -0,0 +1,22 @@
+// name: String,
+// brand: String,
+// code: String,
+// price: Number,
+// //nutricional_info
+// dueDate: Date,
+// insertDate: Date,
+// category: {
+//   type: String,
+//   enum: ["Carne", "Pescado", "Fruta", "Verdura", "Lácteo"]
+// },
+// quantity: Number,
+// status: Boolean,
+// ingredients: String
+
+
+const Products = [
+  {
+    name: "Leche UHT Entera",
+    brand: ""
+  }
+]
