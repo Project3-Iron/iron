e14afe02c35d6b8f2efdfa52366828fbfb0f55cb
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-10 11:25:08 +0200

    Control de errores en el signup

--- client/src/app/AuthSignup/AuthSignup.component.ts
+++ client/src/app/AuthSignup/AuthSignup.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SessionService } from '../services/Session.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { SessionService } from "../services/Session.service";
 
 @Component({
-  selector: 'app-AuthSignup',
-  templateUrl: './AuthSignup.component.html',
-  styleUrls: ['./AuthSignup.component.css']
+  selector: "app-AuthSignup",
+  templateUrl: "./AuthSignup.component.html",
+  styleUrls: ["./AuthSignup.component.css"]
 })
 export class AuthSignupComponent implements OnInit {
-
   formInfo = {
-    username: '',
-    email: '',
-    password: ''
+    username: "",
+    email: "",
+    password: ""
   };
+  error = "";
 
   constructor(public sessionService: SessionService, public router: Router) {}
 
   ngOnInit() {}
 
   signup() {
-    this.sessionService.signup(this.formInfo).subscribe(() => this.router.navigate(['/home']));
+    this.sessionService
+      .signup(this.formInfo)
+      .subscribe(
+        () => this.router.navigate(['/home']),
+        (err) => {return this.error = err}
+      );
   }
 }
