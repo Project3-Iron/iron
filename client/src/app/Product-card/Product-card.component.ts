c9a8430500a99c74c2fa44e8f7767b8abf7f6f74
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-13 12:43:36 +0200

    Compra link  Amazon DONE

--- client/src/app/Product-card/Product-card.component.ts
+++ client/src/app/Product-card/Product-card.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { WindowService } from "../services/Window.service";
 
 @Component({
-  selector: 'app-Product-card',
-  templateUrl: './Product-card.component.html',
-  styleUrls: ['./Product-card.component.css']
+  selector: "app-Product-card",
+  templateUrl: "./Product-card.component.html",
+  styleUrls: ["./Product-card.component.css"]
 })
 export class ProductCardComponent implements OnInit {
-
   @Input() products;
   @Input() status;
   @Output() outGetItem: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  query: String = "";
+  constructor(private windowService: WindowService) {}
 
   ngOnInit() {
     console.log(this.products);
@@ -21,8 +22,15 @@ export class ProductCardComponent implements OnInit {
     this.status = true;
     this.outGetItem.emit(product);
   }
+
+  buyProduct(productName,productBrand) {
+    this.query = `//www.amazon.es/s/ref=nb_sb_noss_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Dgrocery`;
+
+   let keyWords = `&field-keywords=${productName}+${productBrand}`
+
+    this.windowService.nativeWindow.open(`${this.query}${keyWords}`);
+    // $scope.buyProduct = function() {
+    //   $window.open('//facebook.com');
+    // };
+  }
 }
-
-
-
-
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { routes } from './routes';
-import { RouterModule } from '@angular/router';
-import { ClarityModule } from '@clr/angular';
-import { RecipesService } from './services/Recipes.service';
-import { RecipesComponent } from './Recipes/Recipes.component';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './HomeComponent/HomeComponent.component';
-import { AuthSignupComponent } from './AuthSignup/AuthSignup.component';
-import { AuthLoginComponent } from './AuthLogin/AuthLogin.component';
-import { SessionService } from './services/Session.service';
-import { ProductComponent } from './Product/Product.component';
-import { ProductService } from './services/Product.service';
-import { ProductCardComponent } from './Product-card/Product-card.component';
-import { InitialWelcomeComponent } from './OnBoarding/InitialWelcome/InitialWelcome.component';
-import { DeviceService } from './services/Device.service';
-import { LinkDeviceComponent } from './OnBoarding/LinkDevice/LinkDevice.component';
-import { MyDevicesComponent } from './myDevices/myDevices.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { routes } from "./routes";
+import { RouterModule } from "@angular/router";
+import { ClarityModule } from "@clr/angular";
+import { RecipesService } from "./services/Recipes.service";
+import { RecipesComponent } from "./Recipes/Recipes.component";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./HomeComponent/HomeComponent.component";
+import { AuthSignupComponent } from "./AuthSignup/AuthSignup.component";
+import { AuthLoginComponent } from "./AuthLogin/AuthLogin.component";
+import { SessionService } from "./services/Session.service";
+import { ProductComponent } from "./Product/Product.component";
+import { ProductService } from "./services/Product.service";
+import { ProductCardComponent } from "./Product-card/Product-card.component";
+import { InitialWelcomeComponent } from "./OnBoarding/InitialWelcome/InitialWelcome.component";
+import { DeviceService } from "./services/Device.service";
+import { LinkDeviceComponent } from "./OnBoarding/LinkDevice/LinkDevice.component";
+import { MyDevicesComponent } from "./myDevices/myDevices.component";
+import { WindowService } from "./services/Window.service";
 
 @NgModule({
   declarations: [
@@ -43,7 +44,13 @@ import { MyDevicesComponent } from './myDevices/myDevices.component';
     RouterModule.forRoot(routes),
     ClarityModule
   ],
-  providers: [SessionService, DeviceService, ProductService, RecipesService],
+  providers: [
+    SessionService,
+    DeviceService,
+    ProductService,
+    RecipesService,
+    WindowService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
