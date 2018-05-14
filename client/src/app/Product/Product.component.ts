36a7c45e6caa76ecf2fa9dfee7f82078f38edae8
Luis Nueda <nueda.luis@gmail.com>
2018-05-14 15:55:03 +0200

    quantity and price

--- client/src/app/Product/Product.component.ts
+++ client/src/app/Product/Product.component.ts
@@ -9,11 +9,13 @@ import { RecipesService } from "../services/Recipes.service";
   templateUrl: "./Product.component.html",
   styleUrls: ["./Product.component.css"]
 })
+
 export class ProductComponent implements OnInit {
   products$: Observable<any>;
   error = '';
   recipes$: Array<any> = [];
   status$: Boolean;
+  price: Number = 0;
 
   constructor(
     private productService: ProductService,
@@ -25,14 +27,17 @@ export class ProductComponent implements OnInit {
     this.productService
       .getAllProducts()
       .subscribe(
-        products => (this.products$ = products),
+        products => {
+          this.products$ = products;
+          this.sum(this.products$);
+        },
         err => (this.error = err)
       );
   }
 
   getItem(product) {
     this.status$ = true;
-    console.log(this.recipes$)
+    console.log(this.recipes$);
     if (this.recipes$.indexOf(product) === -1) {
       this.recipes$.push(product);
     }
@@ -40,8 +45,15 @@ export class ProductComponent implements OnInit {
   }
 
   deleteItem(product) {
-    console.log(this.recipes$)
+    console.log(this.recipes$);
     this.status$ = false;
     this.recipes$.splice(this.recipes$.indexOf((product) - 1, 1));
+  }
+
+  sum(product) {
+    this.products$.forEach(e => {
+      this.price += e.price;
+      console.log(this.price);
+    });
   }
 }
