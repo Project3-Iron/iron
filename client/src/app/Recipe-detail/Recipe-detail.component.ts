00f7cef7dd27bbca52b8287daa6aed90b592a690
Luis Nueda <nueda.luis@gmail.com>
2018-05-15 18:15:01 +0200

    NAvbar in all componnets

--- client/src/app/Recipe-detail/Recipe-detail.component.ts
+++ client/src/app/Recipe-detail/Recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RecipesService } from "../services/Recipes.service";
 import { WindowService } from "../services/Window.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-Recipe-detail",
@@ -11,7 +12,8 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Object = {};
   constructor(
     private recipesService: RecipesService,
-    private windowService: WindowService
+    private windowService: WindowService,
+    public router: Router
   ) {}
 
   ngOnInit() {
