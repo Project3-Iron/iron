c70ae8f2c44a7fbbfaecd1fba7906d3b123ff323
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-16 13:57:07 +0200

    Añadido tabla en infoPerProduct + cambiado orden imagenes devices

--- server/bin/initial-seed.js
+++ server/bin/initial-seed.js
@@ -6,46 +6,6 @@ const moment = require("moment");
 const Product = require("../models/Product");
 const ProductDB = require("../models/ProductDB");
 
-// const productsIron = [
-//   {
-//     name: "Leche UHT Entera",
-//     brand: "Asturiana",
-//     measure: "1.5L",
-//     code: "25f3d315",
-//     price: 1.25,
-//     dueDate: new Date(2018, 04, 10),
-//     insertDate: new Date(2018, 04, 4),
-//     category: "Lácteo",
-//     quantity: 1,
-//     status: false,
-//     ingredients: "Leche entera de vaca"
-//   }
-// ];
-
-const productsDB = [
-  {
-    name: "Milk Whole",
-    brand: "Asturiana",
-    measure: "1.5L",
-    code: "25f3d315",
-    price: 1.25,
-    dueDate: new Date(2018, 04, 10),
-    //  insertDate: new Date(2018, 04, 4),
-    category: "Lácteo",
-    // quantity: 1,
-    //status: false,
-    ingredients: "Leche entera de vaca"
-  }
-];
-
-// Product.create(productsIron, (err, productsIronDocs) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log(`Created ${productsIronDocs.length} products iron`);
-//   //mongoose.connect.close();
-// });
-
 const remainingDates = date => {
   moment.locale("es");
   let days = moment(date, "YYYYMMDD").fromNow();
