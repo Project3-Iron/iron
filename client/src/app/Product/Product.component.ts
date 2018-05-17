e8bc3184212c0e8d568172289f2f1306eadae5bc
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-17 12:49:07 +0200

    Productos ordenados por expired Days

--- client/src/app/Product/Product.component.ts
+++ client/src/app/Product/Product.component.ts
@@ -4,15 +4,29 @@ import { ProductService } from "../services/Product.service";
 import { Observable } from "rxjs/Rx";
 import { RecipesService } from "../services/Recipes.service";
 
+interface Product {
+  name: String;
+  brand: String;
+  code: String;
+  price: number;
+  measure: String;
+  dueDate: Date;
+  insertDate: Date;
+  category: string;
+  quantity: Number;
+  status: Boolean;
+  ingredients: String;
+  device: String;
+}
+
 @Component({
   selector: "app-Product",
   templateUrl: "./Product.component.html",
   styleUrls: ["./Product.component.css"]
 })
-
 export class ProductComponent implements OnInit {
   products$: Observable<any>;
-  error = '';
+  error = "";
   recipes$: Array<any> = [];
   status$: Boolean;
   price: Number = 0;
@@ -26,18 +40,26 @@ export class ProductComponent implements OnInit {
   ngOnInit() {
     this.productService
       .getAllProducts(this.productService.deviceId)
-      .subscribe(
-        products => {
-          this.products$ = products;
-          this.sum(this.products$);
-        },
-        err => (this.error = err)
-      );
+      .subscribe(products => {
+        this.products$ = this.orderBy(products);
+
+        this.sum(this.products$);
+      }, err => (this.error = err));
+  }
+
+  orderBy(products) {
+    console.log(products);
+    products.sort((a: Product, b: Product) => {
+      let substract = parseInt(b.dueDate) - parseInt(a.dueDate)
+      return substract;
+    });
+
+    return products;
   }
 
   getItem(product) {
     this.status$ = true;
-   // console.log(this.recipes$);
+    // console.log(this.recipes$);
     if (this.recipes$.indexOf(product) === -1) {
       this.recipes$.push(product);
     }
@@ -45,15 +67,15 @@ export class ProductComponent implements OnInit {
   }
 
   deleteItem(product) {
-   // console.log(this.recipes$);
+    // console.log(this.recipes$);
     this.status$ = false;
-    this.recipes$.splice(this.recipes$.indexOf((product) - 1, 1));
+    this.recipes$.splice(this.recipes$.indexOf(product - 1, 1));
   }
 
   sum(product) {
     this.products$.forEach(e => {
       this.price += e.price;
-    //  console.log(this.price);
+      //  console.log(this.price);
     });
   }
 }
--- server/routes/extendedProduct.js
+++ server/routes/extendedProduct.js
@@ -5,13 +5,16 @@ const Product = require("../models/Product");
 const Device = require("../models/Device");
 
 router.get("/:id", (req, res) => {
-  Product.find({ device: req.params.id, status: true }).then(products => {
-    if (products) {
-      return res.status(200).json(products);
-    } else {
-      return res.status(500).json({ message: "Not products for that user" });
-    }
-  });
+  Product.find({ device: req.params.id, status: true })
+    .sort({ dueDate: 1 })
+    .then(products => {
+      if (products) {
+        console.log(products);
+        return res.status(200).json(products);
+      } else {
+        return res.status(500).json({ message: "Not products for that user" });
+      }
+    });
 });
 
 module.exports = router;
